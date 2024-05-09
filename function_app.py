e555d87438edb73c64dd59f1602b20cc67c618e1
Pleava, Dorin <dorin.pleava@porsche-engineering.ro>
2024-05-10 00:41:55 +0300

    test

--- function_app.py
+++
@@ -1,9 +0,0 @@
-import logging
-import azure.functions as func
-
-
-app = func.FunctionApp()
-
-@app.event_grid_trigger(arg_name="azeventgrid")
-def EventGridTriggerMetadataParser(azeventgrid: func.EventGridEvent):
-    logging.info('Python EventGrid trigger processed an event')
