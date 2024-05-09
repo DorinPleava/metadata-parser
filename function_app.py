d06f21b6be4deec0ff7a6a7a617d67f0d85824ff
Pleava, Dorin <dorin.pleava@porsche-engineering.ro>
2024-05-10 01:20:18 +0300

    test

--- function_app.py
+++ function_app.py
@@ -10,3 +10,8 @@ def BlobTrigger(myblob: func.InputStream):
     logging.info(
         f"Python blob trigger function processed blob" f"Name: {myblob.name}" f"Blob Size: {myblob.length} bytes"
     )
+
+
+@app.event_grid_trigger(arg_name="azeventgrid")
+def EventGridTrigger(azeventgrid: func.EventGridEvent):
+    logging.info(f"Python EventGrid trigger function processed event" f"Subject: {azeventgrid.subject}")
