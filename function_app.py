f918390d95434c3215eeff8e8dbe98609de0d4c0
Pleava, Dorin <dorin.pleava@porsche-engineering.ro>
2024-05-10 21:09:14 +0300

    try2

--- function_app.py
+++ function_app.py
@@ -1,14 +1,18 @@
 import logging
 import azure.functions as func
 
+# from azure.eventhub import EventData
+# from azure.eventhub.aio import EventHubProducerClient
+import asyncio
+
 
 app = func.FunctionApp()
 
 
 @app.event_hub_message_trigger(
     arg_name="azeventhub",
-    event_hub_name="myeventhub",
-    connection="eventhubnamespacemetadataparser_RootManageSharedAccessKey_EVENTHUB",
+    event_hub_name="file_uploaded_trigger",
+    connection="pedshub_RootManageSharedAccessKey_EVENTHUB",
 )
-def eventhub_trigger(azeventhub: func.EventHubEvent):
+def file_uploaded_trigger(azeventhub: func.EventHubEvent):
     logging.info('Python EventHub trigger processed an event: %s', azeventhub.get_body().decode('utf-8'))
